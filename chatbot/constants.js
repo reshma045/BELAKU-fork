bee922bc7bf7de99ac8daa3f19dd7bebddf48976
shashanksbharadwaj161 <132832605+shashanksbharadwaj161@users.noreply.github.com>
2023-05-17 22:11:58 +0530

    Add files via upload

---
+++ chatbot/constants.js
@@ -0,0 +1,72 @@
+// Options the user could type in
+const prompts = [
+  ["hi","hai", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what do you do","what can you do"],
+  ["im fine", "im doing good", "im doing great"],
+  ["i will commit suicide","i want to commit suicide","i think about suicide"],
+  ["okay"],
+  ["i love you"],
+  ["please be my friend", "can you be my friend"],
+  ["thank you", "thanks", "thank you so much","thank you very much"],
+  ["im feeling sad", "im sad", "im not okay"],
+  ["im angry", "i feel angry","i feel anxious","im anxious"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  ["I'm here to give you company",
+    "You can share your feelings with me",
+    "I'm here to make you feel great",
+    "I love talking to beautiful humans like you"
+  ],
+  ["Happy to hear that", "Thats what i wish for, happy to hear that!"],
+  ["You're not alone. I may not understand exactly how you feel, but you're not alone",
+    "Dont talk about that please, you have more to go through. You're strong enough",
+    "There are people who really love you, so don't",
+    "Please don't worry about what you are going through, find a way to solve it"
+  ],
+  ["thank you for agreeing with me.", "okay"],
+  ["I love you too", "Me too","Aww , You are the beautiful thing in this whole world , love you too"],
+  ["of course i will", "i would love to","thanks for making me your friend"],
+  ["you are welcome", "glad to help"],
+  ["Come on everthing will be alright soon",
+   "Do things you love to keep you happy"],
+  ["It's okay, now breathe slowly and relax"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
